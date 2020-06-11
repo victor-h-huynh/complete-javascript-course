203d3d9300916c4e002abad39f921a73f841e03c
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-06-10 21:18:09 -0400

    Update script.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,17 +1,46 @@
-var firstName = 'John';
-console.log(firstName);
+/************************************************************
+ * Variables and data types
+ */
 
-var lastName = 'Smith';
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// var _3years = 3;
+// var johnMark = 'John and Mark';
+
+///////////////////////////////////////////////////////////////////
+// Variable mutation and type coercion
+//
+
+var firstName = 'John';
 var age = 28;
 
-var fullAge = true;
-console.log(fullAge);
+// Type coercion
+console.log(firstName + ' ' + age);
 
-var job;
-console.log(job);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
 
-job = 'Teacher';
-console.log(job);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
 
-var _3years = 3;
-var johnMark = 'John and Mark';
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+var lastName = prompt('What is his last Name?')
+console.log(firstName + ' ' + lastName);
