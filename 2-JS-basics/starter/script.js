cbcb4cfe80f3c8c51d73d9fcb74d11a5fbcb914b
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-06-10 21:41:43 -0400

    Update script.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -24,23 +24,52 @@
 // Variable mutation and type coercion
 //
 
-var firstName = 'John';
-var age = 28;
+// var firstName = 'John';
+// var age = 28;
 
-// Type coercion
-console.log(firstName + ' ' + age);
+// // Type coercion
+// console.log(firstName + ' ' + age);
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
 
-// Variable mutation
-age = 'twenty eight';
-job = 'driver';
+// // Variable mutation
+// age = 'twenty eight';
+// job = 'driver';
 
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
 
-var lastName = prompt('What is his last Name?')
-console.log(firstName + ' ' + lastName);
+// var lastName = prompt('What is his last Name?')
+// console.log(firstName + ' ' + lastName);
+
+///////////////////////////////////////////////////////////////////
+// Basic operators
+//
+var now, yearVictor, yearMark;
+now = 2020;
+ageVictor = 27;
+ageMark = 33;
+
+// Math operators
+yearVictor = now - ageVictor;
+yearMark = now - ageMark;
+
+console.log('yearVictor', yearVictor);
+
+console.log('now + 2', now + 2);
+console.log('now * 2', now * 2);
+console.log('now / 10', now / 10);
+
+// Logical operators
+var victorOlder = ageVictor < ageMark;
+console.log(victorOlder)
+
+// typeof operator
+console.log(typeof victorOlder)
+console.log(typeof ageVictor)
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
