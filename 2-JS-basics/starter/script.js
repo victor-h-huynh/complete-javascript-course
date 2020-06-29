8ee73c4f96df52a11f2ed9de39c9ae9486ced9ab
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-06-29 14:25:55 -0400

    Update script.js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -464,3 +464,30 @@
 
 /////////////////////////////////////////////////////////////////
 // Arrays
+
+// Initialize new array
+// var names = ['John', 'Mark', 'Jane'];
+// var years = new Array(1990, true, 'Matt')
+
+// // Mutate array data
+// names[1] = 'Ben';
+// names[names.length] = 'Mary';
+// console.log(names)
+
+// // Different data types
+// var john = ['John', 'Smith', 1990, 'teacher', false]
+
+// john.push('blue');
+// john.unshift('Mr.')
+// console.log(john)
+
+// john.pop();
+// john.pop();
+// john.shift();
+// console.log(john)
+
+// console.log(john.indexOf(23));
+
+// var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer';
+// console.log(isDesigner)
+
