c34d1fca368cef6a9b733df32c94838dd8ba40cb
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-08-03 14:40:12 -0400

    Update app.js

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,7 +11,7 @@ GAME RULES:
 
 let scores = [0 , 0];
 let roundScore = 0;
-let activePlayer = 1;
+let activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -21,7 +21,6 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-
     // 1. Random Number
     let dice = Math.floor(Math.random() * 6) + 1;
 
@@ -31,6 +30,44 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+       nextPlayer();
+    }
+});
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
 
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+
+    // Check if player won the game
+    if(scores[activePlayer] >= 100) {
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); 
+    } else {
+    // Next Player
+        nextPlayer();
+    }
 })
+
+function nextPlayer () {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
